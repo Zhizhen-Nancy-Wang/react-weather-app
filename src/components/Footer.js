0e5d3ae231af6219b7ddb2334e43f746ea05690c
Nancyyy <nancywang.edu@gmail.com>
2022-04-24 21:47:41 +1000

    footer github link added

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,9 @@ import React from "react";
 export const Footer = () => {
   return (
     <div>
-      <a href="#">open-source code</a>
+      <a href="https://github.com/Zhizhen-Nancy-Wang/react-weather-app">
+        open-source code
+      </a>
       by Nancy Wang 24.03.2022
     </div>
   );
